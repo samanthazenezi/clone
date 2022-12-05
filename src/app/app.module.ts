1ba78f58ccbf8a1d40c127e9c9e02d5bbbe46ae1
Samantha Zenezi <samanthazenezi@gmail.com>
2022-12-05 00:27:12 -0300

    banner, marcas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiniMenuComponent } from './components/mini-menu/mini-menu.component';
-import { NavComponent } from './components/nav/nav.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FaixaComponent } from './components/faixa/faixa.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { CupomComponent } from './components/cupom/cupom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiniMenuComponent,
-    NavComponent,
     MenuComponent,
-    FaixaComponent
+    FaixaComponent,
+    BannerComponent,
+    MarcasComponent,
+    SobreComponent,
+    CupomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cupom/cupom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cupom',
+  templateUrl: './cupom.component.html',
+  styleUrls: ['./cupom.component.css']
+})
+export class CupomComponent {
+
+}
